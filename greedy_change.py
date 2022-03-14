6491bc6489889ab7f4067c95675a557a67b40cda
alpakaDurumi <durams@naver.com>
2022-03-14 20:50:45 +0900

    deleted junk files

--- greedy_change.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-coin = [500, 100, 50, 10]
-acc = 0
-for i in coin:
-    acc += n // i
-    n %= i
-print(acc)
--- greedy_numberCard.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split()) # 행과 열의 수 입력받기
-
-smallest = [] # 각 행에서의 가장 작은 수를 저장하기 위한 리스트
-for i in range(n): # 각 행에서 반복(n번 반복)
-	data = map(int, input().split()) # 각 행에 해당하는 수들을 입력받아
-	smallest.append(min(data)) # 해당 행에서 가장 작은 수를 리스트에 append
-
-print(max(smallest)) # 리스트에서 가장 큰 값을 print
